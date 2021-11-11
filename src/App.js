213eb976d403e86493f328432a593cab394d16b1
Holutunde <Holutunde99@gmail.com>
2021-11-11 02:46:16 +0100

    modify App

--- src/App.js
+++ src/App.js
@@ -1,46 +1,36 @@
-import React,{useState } from 'react'
-import './App.css';
-import Header from './component/components/Header';
-import Todoform from './component/components/Todoform';
-import Todolist from './component/components/Todolist';
-//import Todoform from './component/Todoform';
-// import Todolist from './component/Todolist';
-
-
-
+import React, { useState } from 'react'
+import './App.css'
+import Header from './component/components/Header'
+import Todoform from './component/components/Todoform'
+import Todolist from './component/components/Todolist'
 
 export default function App() {
   //const initialState = JSON.parse(localStorage.getItem("todos")) || [];
-  const [input, setInput] = useState(''); 
+  const [input, setInput] = useState('')
 
-	 const[todos, setTodos] =useState([]);
+  const [todos, setTodos] = useState([])
 
-   const[edit, setEdit] = useState(null);
+  const [edit, setEdit] = useState(null)
   return (
- 
     <div className="container">
-          <div className="app-wrapper">
-            <div>
-               <Header/>
-            </div>
-            <div>
-              <Todoform
-              input={input}
-              setInput={setInput}
-              todos={todos}
-              setTodos={setTodos}
-              edit={edit}
-              setEdit={setEdit}
-              />
-            </div>
-            <div>
-              <Todolist
-              todos={todos}
-              setTodos={setTodos}
-              setEdit={setEdit} 
-              />
-            </div>
-          </div>
-   </div>
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Todoform
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            edit={edit}
+            setEdit={setEdit}
+          />
+        </div>
+        <div>
+          <Todolist todos={todos} setTodos={setTodos} setEdit={setEdit} />
+        </div>
+      </div>
+    </div>
   )
 }
